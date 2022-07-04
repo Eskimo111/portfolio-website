34a2a36ba7dcd38d8c914c33d7d6acbdd65dc9d7
phamnguyen <nguyenptse160896@fpt.edu.vn>
2022-07-04 22:35:36 +0700

    Services

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -11,10 +11,69 @@ const Experience = () => {
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className="experience__details"></article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
           </div>
         </div>
-        <div className="experience__backend"></div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>C++</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,116 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+
 const Portfolio = () => {
-  return <section id="portfolio"></section>;
+  return (
+    <section id="portfolio">
+      <h5>My recent work</h5>
+      <h2>Portfolio</h2>
+      <div className="container container__portfolio">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="First project" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="First project" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="First project" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="First project" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="First project" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="First project" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
