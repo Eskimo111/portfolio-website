5ccf34a814edee313b325d18c1e13e1fda6cfe9e
phamnguyen <nguyenptse160896@fpt.edu.vn>
2022-07-14 16:17:37 +0700

    Final commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,108 +6,67 @@ import IMG3 from "../../assets/portfolio3.jpg";
 import IMG4 from "../../assets/portfolio4.jpg";
 import IMG5 from "../../assets/portfolio5.png";
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Crypto Currency Dashboard & Financial Visualization",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Helicopter Animation Tutorial",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/18674371-Helicopter-Animation-Tutorial",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Furniture Studio Landing Page",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/18671573-Furniture-Studio-Landing-Page",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "pourtion: web design, product page, hero header, illustration",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/18667992-pourtion-web-design-product-page-hero-header-illustration",
+  },
+];
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My recent work</h5>
       <h2>Portfolio</h2>
-      <div className="container container__portfolio">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="First project" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="First project" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="First project" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="First project" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="First project" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="First project" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/shots/18671535-DeFi-farming-identity-and-illustrations"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,72 @@
 import React from "react";
 import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/bundle";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Tina Show",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et modi saepe laudantium exercitationem distinctio sunt voluptate numquam, perferendis perspiciatis rem eveniet repellendus at fugiat fuga tenetur! Veritatis modi rem enim.",
+  },
+  {
+    avatar: AVTR2,
+    name: "Shatte Wale",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et modi saepe laudantium exercitationem distinctio sunt voluptate numquam, perferendis perspiciatis rem eveniet repellendus at fugiat fuga tenetur! Veritatis modi rem enim.",
+  },
+  {
+    avatar: AVTR3,
+    name: "Kwame Despite",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et modi saepe laudantium exercitationem distinctio sunt voluptate numquam, perferendis perspiciatis rem eveniet repellendus at fugiat fuga tenetur! Veritatis modi rem enim.",
+  },
+  {
+    avatar: AVTR4,
+    name: "Ellie Khan",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et modi saepe laudantium exercitationem distinctio sunt voluptate numquam, perferendis perspiciatis rem eveniet repellendus at fugiat fuga tenetur! Veritatis modi rem enim.",
+  },
+];
 
 const Testimonials = () => {
-  return <section id="testimonials"></section>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper
+        className="container testimonial__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map((testimonial, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={testimonial.avatar} alt={testimonial.name} />
+              </div>
+              <h5>{testimonial.name} </h5>
+              <small className="client__review">{testimonial.review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonials;
